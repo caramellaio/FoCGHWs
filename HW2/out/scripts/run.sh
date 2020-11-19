92b40cb391b6e795dc0d8c8fd68d91d09cf8d6f7
caramellaio <albertobombardelli123@gmail.com>
2020-11-19 22:48:39 +0100

    Add HW2 + start working on shade_naive

---
+++ HW2/out/scripts/run.sh
@@ -0,0 +1,25 @@
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/naive/01_cornellbox_512_256.jpg -t naive -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/naive/02_matte_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/naive/03_metal_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/naive/04_plastic_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/naive/05_glass_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/naive/06_opacity_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/naive/07_hair_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/08_lens/lens.json -o out/naive/08_lens_720_256.jpg -t naive -s 256 -r 720
+
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/path/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/path/02_matte_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/path/03_metal_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/path/04_plastic_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/path/05_glass_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/path/06_opacity_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/path/07_hair_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/08_lens/lens.json -o out/path/08_lens_720_256.jpg -t path -s 256 -r 720
+
+./bin/ypathtrace tests/11_bathroom1/bathroom1.json -o out/path/11_bathroom1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/12_ecosys/ecosys.json -o out/path/12_ecosys_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/13_bedroom/bedroom.json -o out/path/13_bedroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/14_car1/car1.json -o out/path/14_car1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/15_classroom/classroom.json -o out/path/15_classroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/16_coffee/coffee.json -o out/path/16_coffee_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/17_kitchen/kitchen.json -o out/path/17_kitchen_720_256.jpg -t path -s 256 -r 720
