401e269093cb4c2d3639d3fa85ff141690031a0a
caramellaio <albertobombardelli123@gmail.com>
2020-12-20 23:58:19 +0100

    Add HW3 initial files

---
+++ HW3/out/scripts/run-highres.sh
@@ -0,0 +1,6 @@
+./bin/ypathtrace tests/01_surface/surface.json -o out/highres/01_surface_1280_1024.jpg -t volpath -s 1024 -r 1280
+./bin/ypathtrace tests/02_rollingteapot/rollingteapot.json -o out/highres/02_rollingteapot_1280_1024.jpg -t volpath -s 1024 -r 1280
+./bin/ypathtrace tests/03_volume/volume.json -o out/highres/03_volume_1280_1024.jpg -t volpath -s 1024 -r 1280
+./bin/ypathtrace tests/04_head1/head1.json -o out/highres/04_head1_1280_1024.jpg -t volpath -s 1024 -r 1280
+./bin/ypathtrace tests/05_head1ss/head1ss.json -o out/highres/05_head1ss_1280_1024.jpg -t volpath -s 4096 -r 1280
+
